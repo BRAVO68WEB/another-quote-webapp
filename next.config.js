de7de15e11eb84f6e72e16a0ad0797a07f0b02a8
BRAVO68WEB <jbandyopadhayaya@gmail.com>
2022-06-28 15:47:35 +0530

    Added cdn support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/BRAVO68WEB/another-quote-webapp/gh-pages/' : '',
 };
 
 module.exports = nextConfig;
